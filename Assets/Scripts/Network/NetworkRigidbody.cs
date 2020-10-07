77fb12f43a46d91f489dc00a43a30f8ccbac13c4
kruemelkeksfan <37933687+kruemelkeksfan@users.noreply.github.com>
2020-10-07 21:58:27 +0200

    Introduced Position-Resync-Smoothing for Clients and minor Refactors.

--- Assets/Scripts/Network/NetworkRigidbody.cs
+++ Assets/Scripts/Network/NetworkRigidbody.cs
@@ -7,11 +7,14 @@ public class NetworkRigidbody : NetworkObject
 	[SerializeField] private new Rigidbody rigidbody = null;
 	[SerializeField] private float positionTolerance = 0.4f;
 	[SerializeField] private float rotationTolerance = 10.0f;
+	[SerializeField] private float correctionTime = 1.0f;
 	private Vector3 lastPosition = Vector3.zero;
 	private Vector3 lastRotation = Vector3.zero;
 	private Vector3 lastVelocity = Vector3.zero;
 	private Vector3 lastRotationVelocity = Vector3.zero;
 	private float lastUpdate = 0.0f;
+	private Vector3 dPosition = Vector3.zero;
+	private float remainingCorrectionTime = 0.0f;
 
 	protected override void Start()
 	{
@@ -23,21 +26,32 @@ public class NetworkRigidbody : NetworkObject
 
 	private void FixedUpdate()
 	{
-		if(network != null && network.IsHost)
+		if(network != null)
 		{
-			float dTime = Time.time - lastUpdate;
-			Vector3 predictedPosition = lastPosition + lastVelocity * dTime;
-			Vector3 predictedRotation = lastRotation + lastRotationVelocity * dTime;
-			if((predictedPosition - transform.position).sqrMagnitude > positionTolerance
-				|| Quaternion.Angle(Quaternion.Euler(predictedRotation), transform.rotation) > rotationTolerance)
+			if(network.IsHost)
 			{
-				network.SendPositionUpdate(ID, transform.position, transform.rotation.eulerAngles, rigidbody.velocity, rigidbody.angularVelocity);
+				float dTime = Time.time - lastUpdate;
+				Vector3 predictedPosition = lastPosition + lastVelocity * dTime;
+				Vector3 predictedRotation = lastRotation + lastRotationVelocity * dTime;
+				if((predictedPosition - transform.position).sqrMagnitude > positionTolerance
+					|| Quaternion.Angle(Quaternion.Euler(predictedRotation), transform.rotation) > rotationTolerance)
+				{
+					network.SendPositionUpdate(ID, transform.position, transform.rotation.eulerAngles, rigidbody.velocity, rigidbody.angularVelocity);
 
-				lastPosition = rigidbody.transform.position;
-				lastRotation = rigidbody.transform.rotation.eulerAngles;
-				lastVelocity = rigidbody.velocity;
-				lastRotationVelocity = rigidbody.angularVelocity;
-				lastUpdate = Time.time;
+					lastPosition = rigidbody.transform.position;
+					lastRotation = rigidbody.transform.rotation.eulerAngles;
+					lastVelocity = rigidbody.velocity;
+					lastRotationVelocity = rigidbody.angularVelocity;
+					lastUpdate = Time.time;
+				}
+			}
+			else if(network.IsClient)
+			{
+				if(remainingCorrectionTime > 0)
+				{
+					transform.position += dPosition * Mathf.Min(Time.deltaTime, remainingCorrectionTime);
+					remainingCorrectionTime -= Time.deltaTime;
+				}
 			}
 		}
 	}
@@ -45,8 +59,10 @@ public class NetworkRigidbody : NetworkObject
 	public void UpdatePosition(float positionX, float positionY, float positionZ, float rotationX, float rotationY, float rotationZ,
 		float velocityX, float velocityY, float velocityZ, float rotationVelocityX, float rotationVelocityY, float rotationVelocityZ)
 	{
-		transform.position = new Vector3(positionX, positionY, positionZ);
+		dPosition = (new Vector3(positionX, positionY, positionZ) - transform.position);
+		remainingCorrectionTime = correctionTime;
 		transform.rotation = Quaternion.Euler(rotationX, rotationY, rotationZ);
+
 		rigidbody.velocity = new Vector3(velocityX, velocityY, velocityZ);
 		rigidbody.angularVelocity = new Vector3(rotationVelocityX, rotationVelocityY, rotationVelocityZ);
 	}
